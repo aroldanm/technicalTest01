1284689ab3c6bbc6d4957444b154305ed4055b9c
“aroldan” <alan.roldan@e-campus.uab.cat>
2020-03-01 18:40:16 +0100

    T3-1: Create arch of detail

---
+++ TechnicalTest/TechnicalTest/Core/ProductDetail/Presenter/ProductDetailPresenter.swift
@@ -0,0 +1,10 @@
+//
+//  ProductDetailPresenter.swift
+//  TechnicalTest
+//
+//  Created by Alan Roldán Maillo on 01/03/2020.
+//  Copyright © 2020 Alan Roldán Maillo. All rights reserved.
+//
+
+protocol ProductDetailPresenter {
+}
