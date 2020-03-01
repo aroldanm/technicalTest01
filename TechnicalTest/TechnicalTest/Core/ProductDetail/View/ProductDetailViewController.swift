d619ff97ffa9c8d886defa8dd8b9e1d4dfddfada
“aroldan” <alan.roldan@e-campus.uab.cat>
2020-03-02 00:11:29 +0100

    T3-3: make viewcontroller

--- TechnicalTest/TechnicalTest/Core/ProductDetail/View/ProductDetailViewController.swift
+++ TechnicalTest/TechnicalTest/Core/ProductDetail/View/ProductDetailViewController.swift
@@ -10,31 +10,67 @@ import UIKit
 
 class ProductDetailViewController: UIViewController {
     var presenter: ProductDetailPresenter?
-    private var viewModel: ProductDetailViewModel?
+
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var imageBlurred: UIImageView! {
+        didSet {
+            let effect = UIBlurEffect(style: .regular)
+            let effectView = UIVisualEffectView(effect: effect)
+            effectView.frame = imageBlurred.bounds
+            effectView.autoresizingMask = [.flexibleWidth,
+                                           .flexibleHeight]
+            imageBlurred.addSubview(effectView)
+        }
+    }
+    @IBOutlet private weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.minimumScaleFactor = 0.65
+            titleLabel.adjustsFontSizeToFitWidth = true
+        }
+    }
+    @IBOutlet private weak var genresLabel: UILabel!
+    @IBOutlet private weak var textView: UITextView! {
+        didSet {
+            textView.textContainerInset = UIEdgeInsets(top: 0, left: 12, bottom: 20, right: 12)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        reloadData()
+        setupView()
     }
 }
 
 extension ProductDetailViewController: ProductDetailViewDelegate {
-    func updateView(with model: ProductDetailViewModel) {
-        viewModel = model
-        DispatchQueue.main.async {
-            self.setupView()
-        }
-    }
 }
 
 private extension ProductDetailViewController {
-    func reloadData() {
-        presenter?.loadData()
-    }
-
     func setupView() {
-        guard let viewModel = viewModel else {
+        guard let presenter = presenter else {
             return
         }
+        let viewModel = presenter.loadData()
+        titleLabel.text = viewModel.title
+        genresLabel.text = viewModel.genres
+        
+        if let attributedString = htmlAttributed(from: viewModel.summary) {
+            textView.attributedText = attributedString
+        }
+        if let url = URL(string: viewModel.preview) {
+            imageBlurred.load(from: url)
+        }
+        if let url = URL(string: viewModel.image) {
+            imageView.load(from: url)
+        }
+    }
+
+    func htmlAttributed(from text: String) -> NSAttributedString? {
+        if let font = textView.font {
+            let modifiedFont = String(format:"<span style=\"font-family: '-apple-system', 'HelveticaNeue'; font-size: \(font.pointSize)\">%@</span>", text)
+            if let data = modifiedFont.data(using: .unicode, allowLossyConversion: true) {
+                return try? NSAttributedString(data: data,options: [.documentType: NSAttributedString.DocumentType.html,.characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+            }
+        }
+        return nil
     }
 }
